011fd8c6eec0fb72ff9d3217df8edfdc4d158331
Fmukami <125458349+Fmukami@users.noreply.github.com>
2024-05-06 17:34:05 +0300

    Config.js

--- Config.js
+++ Config.js
@@ -3,11 +3,11 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "254745892196"],
+global.ownername = process.env.OWNER_NAME || "kashata",
+global.ytname = "YT: Kashata.mukami"
+global.socialm = "GitHub: Fmukami"
+global.location = "Kenyan"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
